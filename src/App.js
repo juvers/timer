dceec134fb148f159dfd48332d512759ad52e8e1
Jude K <judekuti@gmail.com>
2021-05-11 02:35:24 -0500

    Complete timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import "./App.css";
+
+function padTime(time) {
+  return time.toString().padStart(2, "0");
+}
 
 function App() {
+  const [timeLeft, setTimeLeft] = useState(600);
+  const [isRunning, setIsRunning] = useState(false);
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const seconds = padTime(timeLeft - minutes * 60);
+  const intervalRef = useRef(null);
+
+  function startTimer() {
+    setIsRunning(true);
+
+    intervalRef.current = setInterval(() => {
+      setTimeLeft((timeLeft) => {
+        if (timeLeft >= 1) {
+          return timeLeft - 1;
+        } else {
+          return 0;
+        }
+      });
+    }, 1000);
+  }
+
+  function stopTimer() {
+    clearInterval(intervalRef.current);
+    setIsRunning(false);
+  }
+
+  function resetTimer() {
+    clearInterval(intervalRef.current);
+    setTimeLeft(600);
+    setIsRunning(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>
+        <u>Timer</u>
+      </h1>
+
+      <div className="timer">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+
+      <div className="buttons">
+        {/* Conditional rendering that removes the start and reset button if the timer
+            is running and removes the stop button if the timer is not running */}
+        {!isRunning && <button onClick={startTimer}>Start</button>}
+        <br />
+        {isRunning && <button onClick={stopTimer}>Stop</button>}
+        {!isRunning && <button onClick={resetTimer}>Reset</button>}
+      </div>
     </div>
   );
 }
